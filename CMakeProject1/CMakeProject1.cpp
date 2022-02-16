f9621c7e9effbe04f0d65f6f55def7bc61506a27
morpheus <albertofots@gmail.com>
2022-02-16 17:00:00 +0100

    Aggiungere i file di progetto.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp: definisce il punto di ingresso dell'applicazione.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CMakeProject1/CMakeProject1.h
@@ -0,0 +1,8 @@
+﻿// CMakeProject1.h: file di inclusione per file di inclusione di sistema standard,
+// o file di inclusione specifici del progetto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Fare riferimento qui alle intestazioni aggiuntive richieste dal programma.
